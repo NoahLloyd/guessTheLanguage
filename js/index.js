04cbb59c7b822eda4d042b59c5bd641a55506019
Noah Lloyd <noahlloydrobson@gmail.com>
2021-09-10 18:25:32 +0200

    Added settings

--- js/index.js
+++ js/index.js
@@ -1,30 +1,6 @@
 const correct = document.getElementById("correct");
 const incorrect = document.getElementById("incorrect");
-const langaugeAmountChoiceDisplay = document.getElementById("langaugeAmountChoiceDisplay");
-const langaugeAmountChoice = document.getElementById("languageAmountChoice");
 
-let language = "Danish";
-
-const updateLanguageAmountDisplay = (value) => {
-  langaugeAmountChoiceDisplay.textContent = value;
-  langaugeAmountChoiceDisplay.textContent += `: ${valueDifficulty(parseInt(value))}`;
-};
-
-const valueDifficulty = (value) => {
-  if (value < 15) {
-    return "Easy";
-  }
-  if (value < 25) {
-    return "Medium";
-  }
-  if (value < 50) {
-    return "Hard";
-  }
-  if (value < 75) {
-    return "Extreme";
-  }
-  return "Ultimate";
-};
 
 const languageGuess = (languageGuessed) => {
   if (languageGuessed === language) {
---
+++ js/settings.js
@@ -0,0 +1,38 @@
+const languageAmountChoiceDisplay = document.getElementById("languageAmountChoiceDisplay");
+const languageAmountChoice = document.getElementById("languageAmountChoice");
+const languageAmountDifficulty = document.getElementById('languageAmount-difficulty')
+const languageAmountDisplayContainer = document.getElementById('languageAmountDisplay-container')
+
+let language = "Danish";
+const updateLanguageAmountDisplay = (value) => {
+    // Handle attempts of setting less than 3 languages
+    if (parseInt(value) < 3) {
+        languageAmountChoice.value = '3';
+        languageAmountChoiceDisplay.classList.add('language-choice-error')
+        languageAmountDisplayContainer.classList.add('language-amount-error')
+        setTimeout(() => {
+            languageAmountChoiceDisplay.textContent = languageAmountChoice.value
+            languageAmountDifficulty.textContent = valueDifficulty(parseInt(value))
+            languageAmountChoiceDisplay.classList.remove('language-choice-error')
+
+        }, 2000)
+    }
+    // 
+    languageAmountChoiceDisplay.textContent = value;
+};
+
+const valueDifficulty = (value) => {
+    if (value < 15) {
+        return "easy";
+    }
+    if (value < 25) {
+        return "medium";
+    }
+    if (value < 50) {
+        return "hard";
+    }
+    if (value < 75) {
+        return "extreme";
+    }
+    return "ultimate";
+};
