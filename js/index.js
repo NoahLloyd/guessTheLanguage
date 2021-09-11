b9d6ec1729eb51c79236ccf0844da6d1e8c288a1
Noah Lloyd <noahlloydrobson@gmail.com>
2021-09-11 09:35:41 +0200

    Box shadows and sound effects

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const correct = document.getElementById("correct");
 const incorrect = document.getElementById("incorrect");
+const correctSound = new Audio('../music/correct.mp3')
+const incorrectSound = new Audio('../music/incorrect.mp3')
 
 
 const languageGuess = (languageGuessed) => {
@@ -11,8 +13,30 @@ const languageGuess = (languageGuessed) => {
 };
 
 const displayVerificationOfGuess = (symbol) => {
+  symbol === correct ? correctSound.play() : incorrectSound.play()
   symbol.classList.add("show");
   setTimeout(() => {
     symbol.classList.remove("show");
   }, 500);
 };
+
+const translateText =  async (text) => {
+  const translation = await fetch("https://google-translate1.p.rapidapi.com/language/translate/v2", {
+	"method": "POST",
+	"headers": {
+		"content-type": "application/x-www-form-urlencoded",
+		"accept-encoding": "application/gzip",
+		"x-rapidapi-host": "google-translate1.p.rapidapi.com",
+		"x-rapidapi-key": "4e61f42c50msha70b430044d92a4p109d6fjsnec3420902daf"
+	},
+	"body": {
+		"q": "Hello, world!", 
+		"target": "dk",
+		"source": "en"
+	}
+})
+const translationJson = await translation.json()
+console.log(translationJson)
+}
+
+// translateText('asd')
