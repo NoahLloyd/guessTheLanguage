3530754502885f09b5b151dea50d3ceec40538e8
Noah Lloyd <noahlloydrobson@gmail.com>
2021-09-22 07:14:53 +0200

    removed rapidapi key

--- js/getText.js
+++ js/getText.js
@@ -11,7 +11,7 @@ const translateText = async (text, language = "dk") => {
           "accept-encoding": "application/gzip",
           "x-rapidapi-host": "google-translate1.p.rapidapi.com",
           "x-rapidapi-key":
-            "4e61f42c50msha70b430044d92a4p109d6fjsnec3420902daf",
+            "",
         },
         body: {
           q: "Hello, world!",
